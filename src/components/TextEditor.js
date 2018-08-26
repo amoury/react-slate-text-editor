ccbff2a42f997414b27b2c652a7db17f7b63c4d4
Amoury <ansar.memon@hotmail.com>
2018-08-26 16:02:24 +0400

    Initial Commit with slate setup :tada:

---
+++ src/components/TextEditor.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
+import BoldMark from './BoldMark';
+
+const initialValue = Value.fromJSON({
+  document: { 
+    nodes: [
+      { 
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'My First Paragraph',
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+})
+
+class TextEditor extends Component {
+
+  state = {
+    value: initialValue
+  }
+
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
+  onKeyDown = (e, change) => {
+    if(!e.ctrlKey) return;
+    e.preventDefault();
+
+    switch (e.key) {
+      case 'b': 
+        change.toggleMark('bold')
+        return true
+      default:
+        return;
+    }
+  }
+
+  renderMark = props => {
+    switch (props.mark.type) {
+      case 'bold': 
+        return <BoldMark {...props} />
+    }
+  }
+
+  render () {
+    return (
+      <Editor value={this.state.value} onChange={this.onChange} onKeyDown={this.onKeyDown} renderMark={this.renderMark} />      
+    );
+  }
+}
+
+export default TextEditor;
